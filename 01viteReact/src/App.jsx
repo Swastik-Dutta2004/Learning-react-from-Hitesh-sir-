dff02e230b10ece093d6b50194faaa15737422a2
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-02-15 01:04:43 +0530

    vdo number 3 has complited by hitech sir.

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -3,14 +3,12 @@ import Chai from "../chia"
 
 
 function App() {
-  
+const username = "chai aur code" 
 
   return (
     <>
     <Chai/>
-    
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quos expedita facilis quod at fugiat amet, ullam molestias maiores iste vitae nemo voluptatum odio aperiam accusamus quam laboriosam officiis aliquid tempora? Magni dolor obcaecati eaque expedita corrupti. Doloribus veritatis sed quam, beatae est velit optio!</p>
-    <h2>chai aur react</h2>
+    <h1>React learning from {username}</h1>
     </>
   )
 }
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,8 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    // return(
+    //     // <div>
+    //     //     <h1>custom app</h1>
+    //     // </div>
+    // )
+}
+const anotherElement = (
+    <a href='https://google.com' target = "-blank">visitgoogle</a>
+)
+
 createRoot(document.getElementById('root')).render(
 
-    <App />
-  
+    <App/>
 )
