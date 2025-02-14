48203351481c4f57ce2f11bbb500e7ec70a93992
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-02-14 11:21:44 +0530

    Trying stay consistancy.

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,3 +1,4 @@
+import Chai from "../chia"
 
 
 
@@ -5,7 +6,12 @@ function App() {
   
 
   return (
-    <h1>chai aur code with vite |swastik</h1>
+    <>
+    <Chai/>
+    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quos expedita facilis quod at fugiat amet, ullam molestias maiores iste vitae nemo voluptatum odio aperiam accusamus quam laboriosam officiis aliquid tempora? Magni dolor obcaecati eaque expedita corrupti. Doloribus veritatis sed quam, beatae est velit optio!</p>
+    <h2>chai aur react</h2>
+    </>
   )
 }
 
