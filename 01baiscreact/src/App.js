31d84ce1dac8e9eaf2b72be7c0d3fc830b83e3ab
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-02-14 13:17:50 +0530

    Understand the react flow and structure

--- 01baiscreact/src/App.js
+++ 01baiscreact/src/App.js
@@ -1,13 +1,11 @@
 
-
+import Chai from "./Chai";
 function App() {
   return (
-    <h1>chai aur react | Myself Swastik dutta 
-      At last I started learning React. By this project I have learned how to build a file by terminal. There I faced some npm problems, which I fixed with ChatGPT. Then how to open the host page in browser, how to change the element in that host then again i face problem which was again fixed by Mr. chatGPT. All of this gives me excitement about learning it more deeply, and I am eager to learn it.
-
-
-        And this is all I learned from Chai aur code by Hitesh Sir.
-    </h1>
+    <>
+    <Chai/>
+    
+    </>
   );
 }
 
