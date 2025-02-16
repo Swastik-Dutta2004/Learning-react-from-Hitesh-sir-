7b8f3bd2afc665427835db40e28df1d8282d80f0
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-02-16 12:20:58 +0530

    Project setup completed.

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from '../components/card'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>tailwind class</h1>
+    <h1 className='bg-green-500 text-white p-4 rounded-xl mb-4'>fuck you</h1>
+
+ 
+     <Card />   
     </>
   )
 }
