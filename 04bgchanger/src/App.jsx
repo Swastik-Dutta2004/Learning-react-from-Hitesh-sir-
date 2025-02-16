ae64ef38e4de5bdb1c1e4a3ba9f4bd0af3b1e117
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-02-16 22:11:54 +0530

    Porject completd.

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -7,7 +7,33 @@ function App() {
    <>
 <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
   <div className='fixed flex flex-wrap justify-center bottom-20 px-90 '>
-  <div className='flex flex-wrap justify-center gap-2.5 shadow-amber-400 bg-white px-2.5 py-2.5 rounded-xl'>test</div>
+
+  <div className='flex flex-wrap justify-center gap-2.5 shadow-amber-400 bg-black px-2.5 py-2.5 rounded-2xl'>
+
+    <button 
+     onClick={()=>setColor("red")}
+    className='outline-none px-3 py-1 rounded-full text-black shadow-lg '
+    style={{backgroundColor:"red"}}
+    >red</button>
+
+    <button 
+     onClick={()=>setColor("yellow")} 
+    className='outline-none px-3 py-1 rounded-full text-black shadow-lg '
+    style={{backgroundColor:"yellow"}}
+    >yellow</button>
+
+    <button
+    onClick={()=>setColor("green")} 
+    className='outline-none px-3 py-1 rounded-full text-black shadow-lg '
+    style={{backgroundColor:"green"}}
+    >green</button>
+    
+    <button
+    onClick={()=>setColor("blue")} 
+    className='outline-none px-3 py-1 rounded-full text-black shadow-lg '
+    style={{backgroundColor:"blue"}}
+    >blue</button>
+  </div>
   </div>
 </div>
    </>
