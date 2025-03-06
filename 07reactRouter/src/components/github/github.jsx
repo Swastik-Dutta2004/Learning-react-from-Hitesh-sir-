3aad2599ddee30706920f7b5b41076e0867b9b0d
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-06 23:31:25 +0530

    working and learning going together.

--- 07reactRouter/src/components/github/github.jsx
+++ 07reactRouter/src/components/github/github.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function github() {
+    const[data,setdata] = useState([])
+    useEffect(() => {
+    featch('https://api.github.com/user/Swastik-dutta2004')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        setdata(data)
+    })
+    }, [])
+     
   return (
-    <div>github</div>
+    <div className='bg-gray-600 text-center text-white m-4'>github follower:{data.follower}
+    </div>
   )
 }
 
