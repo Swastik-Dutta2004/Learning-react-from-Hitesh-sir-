9df5e43e13dc85051e7d759c8bd94d769132dc1f
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-08 19:35:31 +0530

    learn how to use Provider

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
   
   return (
-   <>
    
-   <h1>React with Swastik</h1></> 
+   <UserContextProvider>
+   <h1>React with Swastik</h1>
+   </UserContextProvider>
+    
   )
 }
 
--- 08miniContext/src/Context/UserContextProvider.jsx
+++ 08miniContext/src/Context/UserContextProvider.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Children, useState } from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = () =>{
+    const[user,setuser] = useState(null)
 return(
     <>
-    
+    <UserContext.Provider value={{user,setuser}}>
+        {Children}
+    </UserContext.Provider>
     </>
     )
 }
