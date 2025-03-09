d137eefd98879ba17f3b78c726af44c3a5a70308
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-09 11:19:22 +0530

    Comming end to the project.

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { ThemePorvider } from './context/theme'
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './component/ThemeBtn'
+import Card from './component/Card'
 
 function App() {
- const {thememode,setthememode} = useState("light")
+
+ const {themeMode,setThemeMode} = useState("light")
 
  const lightmode = () => {
-  setthememode("light")
+  setThemeMode("light")
+ }
+ const darkmode = () => {
+  setThemeMode("dark")
  }
 
- const darkmode = () => {
-  setthememode("dark")
- }
+ useEffect( () => {
+  document.querySelector("html").classList.remove("light","dark")
+  document.querySelector("html").classList.add(themeMode)
+ },[themeMode])
 
   return (
- <ThemePorvider value={{thememode,darkmode,lightmode}}>
+ <ThemeProvider value={{themeMode,darkmode,lightmode}}>
  <div className="flex flex-wrap min-h-screen items-center">
   <div className="w-full">
       <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-        {/*themebtn*/}   
+       <ThemeBtn/>
       </div>
 
       <div className="w-full max-w-sm mx-auto">
-          {/*card*/}
+          <Card/>
       </div>
   </div>
 </div>
-</ThemePorvider>
+</ThemeProvider>
   )
 }
 
