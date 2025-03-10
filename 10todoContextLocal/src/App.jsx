e38678c337dfd792c26213d11e1325fc68436937
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-10 21:56:43 +0530

    Doing the last portion

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './Context/TodoContext'
+import TodoForm from './Component/TodoFrom'
 
 function App() {
   const [Todos,setTodos] = useState([])
@@ -23,6 +24,18 @@ function App() {
     setTodos((prev) => prev.map ((prevTodo) => prevTodo === id ? {...prevTodo,completed: !prevTodo.completed} : prevTodo ))
   }
 
+  useEffect(() =>{
+    const todos = JSON.parse(localStorage.getItem("Todos"))
+
+    if(todos && todos.lenght > 0){
+      setTodos(Todos)
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem("Todos", JSON.stringify(Todos))
+  },[Todos])
+
   return (
     <TodoProvider value={{Todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
@@ -30,9 +43,11 @@ function App() {
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
         <div className="mb-4">
             {/* Todo form goes here */} 
+            <TodoForm/>
         </div>
         <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            
         </div>
     </div>
 </div>
---
+++ 10todoContextLocal/src/Component/Index.js
@@ -0,0 +1,4 @@
+import TodoFrom from "./TodoFrom";
+import Todoitem from "./Todoitem";
+
+export {TodoFrom,Todoitem}
