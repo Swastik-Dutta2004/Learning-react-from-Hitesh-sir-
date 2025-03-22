7a8b2cb596b201c0f560e02e1c2fd564c92af9cb
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-22 20:48:20 +0530

    Making authentication folder

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,3 +1,42 @@
 import conf from "../conf/Conf";
 
 import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+        
+        constructor(){
+            this.client
+                setEndpoint(conf.appwriteurl)
+                setProject(conf.appwriteProjectId);
+                this.account = new Account(this.client);
+        }
+
+        async CreateAccount ({Email,Password,Name}){
+            try {
+               const userAccount= await this.account(ID.unique(),Email,Password,Name);
+                if (userAccount) {
+                    return this.Login
+                }else{
+                    return userAccount;
+                }
+            } catch (error) {
+                throw error
+            }
+        }
+
+        async Login ({Email,Password}){
+            try {
+                return await this.account.createEmailPasswordSession(Email,Password)
+            } catch (error) {
+                throw error
+            }
+        }
+
+        }
+
+
+const authService = new AuthService();
+
+export default authService
