9d72a88f48ea96f0c91f4a05f279761773bf8890
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-23 19:15:23 +0530

    create post part has completed.

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -27,43 +27,7 @@ export class Service{
                 console.log("Appwrite serive :: GetCurrentUser ::error", error);
             }
         }
-        async updatePost (slug,{title,content,featureImage,status,userId}){
-            try {
-              return await this.databases.updateDocument(
-                conf.appwriteDataBaseId,
-                conf.appwriteCollectionId,
-                slug,
-                {title,content,featureImage,status,}
-              )  
-            } catch (error) {
-                console.log("Appwrite serive :: GetCurrentUser ::error", error);
-            }
-        }
-        async deletePost (slug){
-            try {
-               await this.databases.deleteDocument(
-                conf.appwriteDataBaseId,
-                conf.appwriteCollectionId,
-                slug
-               ) 
-               return true;
-            } catch (error) {
-                console.log("Appwrite serive :: GetCurrentUser ::error", error);  
-                return false;
-            }
-        }
-        async getPost(slug){
-            try {
-                return await this.databases.getDocument(
-                    conf.appwriteDataBaseId,
-                    conf.appwriteCollectionId,
-                    slug
-                )
-            } catch (error) {
-                console.log("Appwrite serive :: GetCurrentUser ::error", error);  
-                return false
-            }
-        }
+        
 }
 
 const service = new Service()
