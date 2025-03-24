43a7ccc6749236fea8ca5b0a9487416092f010a5
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-24 20:29:50 +0530

    Login and Logout authentication has completed

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux';
 import authService from "./appwrite/auth"
-
+import {login,logout} from "./store/authSlice"
 
 function App() {
 const [loading,setloading] = useState(true);
 const dispatch = useDispatch()
+
+useEffect (() => {authService.GetCurrentUser().then((useData) =>{
+  if (useData) {
+    dispatch(login({useData}))
+  } else {
+    dispatch(logout())
+  }
+})
+.finally()
+},[])
   return (
    <>
    <h1>Started doing a Responsive project </h1>
