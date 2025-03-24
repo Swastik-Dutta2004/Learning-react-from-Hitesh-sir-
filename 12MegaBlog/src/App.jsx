e851ec8d27ef81a2466130d061d898491675438a
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-24 20:16:53 +0530

    Preparing the App.jsx file.

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth"
+
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
-
+const [loading,setloading] = useState(true);
+const dispatch = useDispatch()
   return (
    <>
    <h1>Started doing a Responsive project </h1>
