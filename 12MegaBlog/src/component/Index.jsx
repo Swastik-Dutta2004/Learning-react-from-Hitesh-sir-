ae49b8f9f44a549bdf4524b51f3c1aabe9f5b6de
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-26 22:03:24 +0530

    making Logout function in Header

--- 12MegaBlog/src/component/Index.jsx
+++ 12MegaBlog/src/component/Index.jsx
@@ -1,4 +1,7 @@
 import header from "./compnent/Header/Header";
 import Footer from "./component/Footer/Footer";
+import container from './container/container'
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export {header,Footer}
+export {header,Footer,container,Logo,LogoutBtn}
