40177b6fae3e56692fe29b2657a9bfe0335e052c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-03-29 08:59:18 +0530

    Post Card is completed.

--- 12MegaBlog/src/component/PostCard.jsx
+++ 12MegaBlog/src/component/PostCard.jsx
@@ -2,13 +2,19 @@
  import appwriteService from "../appwrite/config"
 import { Link } from 'react-router-dom'
 
- function PostCard({$id, title,freaturedImage}) {
+ function PostCard({$id, title,featureImage}) {
 
    return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl'></div>
+        <div className='w-full bg-gray-100 rounded-xl'>
+          <div className='w-full justify-center mb-4'>
+            <img src={appwriteService.getfilePreview(featureImage)} alt={title}
+            className='rounded-xl'></img>
+          </div>
+          <h2 className='text-xl font-bold'></h2>
+        </div>
     </Link>
    )
  }
  
- export default PostCard
+ export default PostCard 
